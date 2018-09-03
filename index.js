43a2546797c774c72a0b5cf81e4ce194b846ad79
Marissa So <sotom591@gmail.com>
2018-09-03 04:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ function preserveDecimal(n)
 {
   return parseFloat ('2.222');
 }
+ console.log(preserveDecimal('sldkjflksjf'));
 function preserveDecimal(n)
 {
  return parseInt ('sldkjflksjf', 10);
