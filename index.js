b4434b8972b53b20441d95bef8850767e2326086
Marissa So <sotom591@gmail.com>
2018-09-03 04:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,15 +38,6 @@ function dec(a)
 
 
 
-var n
-n = Math.floor(Math.random() * 1000)
-function assumeBaseTen()
-{
-  return (0*2328);
-function nan()
-{
-  return parseInt ('sldkjflksjf', 10);
-}
 
 
 function makeInt(n)
