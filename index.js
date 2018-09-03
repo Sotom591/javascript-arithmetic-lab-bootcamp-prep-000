5b141c455f0cc02822ad9aafbcfe7ee3edaadede
Marissa So <sotom591@gmail.com>
2018-09-03 04:23:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,8 +46,11 @@ function makeInt(n)
   return parseInt (n, 10);
 }
 console.log(makeInt(0*2328));
-
-
+console.log(makeInt('sldkjflksjf',10));
+ 
+function assumeBaseTen()
+{
+  return (0*2328);
 function nan()
 {
   return parseInt ('sldkjflksjf', 10);
