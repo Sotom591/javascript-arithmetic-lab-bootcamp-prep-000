9186aac66c4eed64a87d7b4813c60265c97eb5ab
Marissa So <sotom591@gmail.com>
2018-09-03 00:48:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,67 @@
+var a = Math.floor(Math.random() * 1000);
+var b = Math.floor(Math.random() * 1000);
+
+function add(a, b)
+{
+ return a + b; 
+}
+
+
+function subtract(a, b)
+{
+  return a-b;
+}
+
+function multiply(a, b)
+{
+  return a*b;
+}
+
+
+function divide(a, b)
+{
+  return a/b;
+}
+
+
+function inc(a)
+{
+  return a +=1;
+}
+
+incn();
+
+function dec(a)
+{
+  return a -=1;
+}
+
+
+
+var n
+n = Math.floor(Math.random() * 1000)
+
+function makeInt(n)
+{
+  return parseInt (n, 10);
+}
+
+function assumeBaseTen()
+{
+  return (0*2328);
+
+}
+
+
+function nan()
+{
+  parseInt ('sldkjflksjf', 10);
+}
+
+
+
+function preserveDecimal(n)
+{
+ return parseFloat ('2.222');
+ 
+}
